e0cf428c848259d8cce62475d60aee8ca78c9237
Diego Sanchez <diegodanchez24@gmail.com>
2026-01-20 00:13:44 -0600

    feat: agregar calculo de iva

--- GitConflictoSanchezDiego/src/Main.java
+++ GitConflictoSanchezDiego/src/Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
+    static double IVA = 0.16;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
-        double total = subtotal;
+        double total = calcularTotal(subtotal);
 
         System.out.printf("Total a pagar: %.2f%n", total);
     }
@@ -14,5 +16,9 @@ public class Main {
     public static double pedirDouble(Scanner scanner, String mensaje) {
         System.out.print(mensaje);
         return scanner.nextDouble();
+    }
+
+    public static double calcularTotal(double subtotal){
+        return subtotal + (subtotal * IVA);
     }
 }
