f1d55aa8124b42881da5523ca2713c1f96051eda
Diego Sanchez <diegodanchez24@gmail.com>
2026-02-03 01:33:14 -0600

    Avance de practica

---
+++ PracticaNumRandom/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static int contadorAciertos;
+    public static int contadorNoNumericos;
+    public static void main(String[] arga){
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int ronda = 1; ronda == 5; ronda++){
+
+
+        }
+    }
+
+    public static int ontenerEntero(Scanner scanner, int  )
+
+    public static int validarNumero(){
+
+    }
+
+}
